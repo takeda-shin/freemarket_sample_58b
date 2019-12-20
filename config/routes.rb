9e44112eb9043bd340429bfed97caf7593d133ec
takeda-shin <kgcfy958@yahoo.co.jp>
2019-12-21 03:44:11 +0900

    add signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :signup do
+    collection do
+      get 'index' #登録方法選択
+      get 'step1' #会員情報
+      get 'step2_1' #電話番号認証
+      get 'step2_2'
+      get 'step3' #お届け先
+      get 'step4' #支払い方法
+      get 'finish' #登録完了ページ
+      
+      get 'login' #ログイン
+
+    end
+  end
 end
