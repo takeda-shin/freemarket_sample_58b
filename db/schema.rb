a27ad8a2c7e991512c8965429a32fa5bc3e82f34
takeda-shin <kgcfy958@yahoo.co.jp>
2019-12-29 16:42:05 +0900

    edit select_box of birthday

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_064714) do
+ActiveRecord::Schema.define(version: 2019_12_29_074004) do
 
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -38,9 +38,7 @@ ActiveRecord::Schema.define(version: 2019_12_29_064714) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "birthday_year", null: false
-    t.integer "birthday_month", null: false
-    t.integer "birthday_date", null: false
+    t.integer "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
