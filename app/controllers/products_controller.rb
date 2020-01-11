538901b68cb34538d09ef22181730b249131ad04
saiesaire <rozenkreuz1126@gmail.com>
2020-01-11 14:25:19 +0900

    last products-show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
   
   def index
-    @categorys = Product.where(category_id: 1).order("rand()").limit(5)
-    @brands    = Product.where(brand_id: 4).order("rand()").limit(5)
+    @categorys = Product.where(category_id: 2).order("rand()").limit(5)
+    @brands    = Product.where(brand_id: 6).order("rand()").limit(5)
   end
 
   def show
