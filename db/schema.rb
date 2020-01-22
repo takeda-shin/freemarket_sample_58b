d36ba8ac2b52dfd8404b79351481578fb89ce3ff
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-22 15:01:55 +0900

    edit birthday column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_000358) do
+ActiveRecord::Schema.define(version: 2020_01_19_095110) do
 
   create_table "card_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
