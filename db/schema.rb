4e2f2596735bdf26ff16681bedf8eab7b5098c6e
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-23 19:35:03 +0900

    edit view use limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_112259) do
+ActiveRecord::Schema.define(version: 2020_01_23_082353) do
 
   create_table "card_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
