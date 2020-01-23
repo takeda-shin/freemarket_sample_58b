0a69de611cb3b90767157fe7e40132f863e6db93
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-24 01:24:26 +0900

    change column of birthday

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_082353) do
+ActiveRecord::Schema.define(version: 2020_01_23_161053) do
 
   create_table "card_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2020_01_23_082353) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "birthday", null: false
+    t.date "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
