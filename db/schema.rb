ae2a950253ef01739b3f5a547590c6dbff89d718
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-23 17:14:30 +0900

    change column of use_limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_095110) do
+ActiveRecord::Schema.define(version: 2020_01_22_112259) do
 
   create_table "card_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "card_number", null: false
-    t.integer "use_limit", null: false
     t.integer "security_code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "use_limit_month", null: false
+    t.integer "use_limit_year", null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
