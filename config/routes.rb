57adb277b355f6918b0857ae68a0398e6de11999
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-25 07:10:52 +0900

    add routes for signup&login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'signup',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
+
   resources :signup do
     collection do
       get 'index' #登録方法選択
       get 'step1' #会員情報
       get 'step2_1' #電話番号認証
+      post 'step2_1' => 'signup#sms_post'
       get 'step2_2'
-      get 'step3' #お届け先
-      get 'step4' #支払い方法
+      post 'step2_2' => 'signup#sms_check'
+      post 'step3' #お届け先
+      post 'step4' #支払い方法
       get 'finish' #登録完了ページ
-      
-      get 'login' #ログイン
-
     end
   end
 
@@ -19,5 +28,9 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :making, only: [:index]
-  resources :users, only: [:index, :new, :show, :edit]
+  resources :users, only: [:index, :new, :show, :edit] do
+    collection do
+      get 'identification'
+    end
+  end
 end
