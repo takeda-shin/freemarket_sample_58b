4d7c360a2d0c92432fe6dd22c4f29a04b6e9056b
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-25 12:35:02 +0900

    modification of validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,19 @@ class User < ApplicationRecord
   has_one :card_info
   accepts_nested_attributes_for :card_info
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimam: 7, maximum: 128 }
-  validates :family_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
-  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
-  validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :birthday, presence: true
+  validates :nickname,
+            :email,
+            :password,
+            :family_name,
+            :first_name, 
+            :family_name_kana,
+            :first_name_kana,
+            :birthday,
+            presence: true
+  validates :password, length: { minimam: 7, maximum: 128 }
+  validates :family_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :family_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
 
 end
--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -12,8 +12,10 @@ class UserAddress < ApplicationRecord
     福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47
   }
 
-  validates :postal_code, presence: true
-  validates :prefectures, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
+  validates :postal_code, 
+            :prefectures,
+            :city,
+            :address,
+            presence: true
+            
 end
