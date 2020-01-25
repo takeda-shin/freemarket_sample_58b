a1005ba5b514b524075f36b48bd773b32b00a180
takeda-shin <kgcfy958@yahoo.co.jp>
2020-01-25 10:53:13 +0900

    Update user_address.rb

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -11,4 +11,9 @@ class UserAddress < ApplicationRecord
     徳島県:36,香川県:37,愛媛県:38,高知県:39,
     福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47
   }
+
+  validates :postal_code, presence: true
+  validates :prefectures, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
 end
