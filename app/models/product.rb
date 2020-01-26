a8e0906e4266377d3791fa8b87fa58bb036072f3
saiesaire <rozenkreuz1126@gmail.com>
2020-01-26 18:47:01 +0900

    fix-again

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,15 @@
 class Product < ApplicationRecord
 
   validates :name,
-            # :image,
-            # :text, 
-            # :category_id,
-            # :condition, 
-            # :product_size ,
-            # :shipping_charge,
-            # :shipping_method, 
-            # :delivery_area, 
-            # :price,
+            :image,
+            :text, 
+            :category_id,
+            :condition, 
+            :product_size ,
+            :shipping_charge,
+            :shipping_method, 
+            :delivery_area, 
+            :price,
             presence: true
   belongs_to :category
   belongs_to :brand
