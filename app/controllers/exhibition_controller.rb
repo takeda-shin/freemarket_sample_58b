c4c03730f17d8054f0854033bb26d691f1ad3614
saiesaire <rozenkreuz1126@gmail.com>
2020-01-26 18:58:34 +0900

    comment-out delete

--- app/controllers/exhibition_controller.rb
+++ app/controllers/exhibition_controller.rb
@@ -3,7 +3,6 @@ class ExhibitionController < ApplicationController
     @products = Product.all
     respond_to do |format| 
       format.html
-      #{ redirect_to products_path(@product) }
       format.json
     end
   end
