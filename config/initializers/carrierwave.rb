cd180b6059ba7b6d10a352105643c10256259e11
takeda-shin <kgcfy958@yahoo.co.jp>
2020-02-08 16:15:57 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,18 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1'
+  }
+#   config.fog_credentials = {
+#     provider: 'AWS',
 
 # CarrierWave.configure do |config|
 #   config.storage = :fog
@@ -11,18 +23,6 @@
 #     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
 #     region: 'ap-northeast-1'
 #   }
-#   config.fog_credentials = {
-#     provider: 'AWS',
-
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-#     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-#     region: 'ap-northeast-1'
-#   }
 
 #   config.fog_credentials = {
 #     provider: 'AWS',
@@ -32,6 +32,6 @@
 #     region: 'ap-northeast-1'
 #   }
 
-#   config.fog_directory  = 'freemarket-sample-58b'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-58b'
-# end
+  config.fog_directory  = 'freemarket-sample-58b'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-58b'
+end
