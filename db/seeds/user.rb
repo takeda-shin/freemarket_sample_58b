3fdb439365a9fab0ae158d35c7d3c983590f28c8
saiesaire <rozenkreuz1126@gmail.com>
2020-02-15 14:10:19 +0900

    user_seed

---
+++ db/seeds/user.rb
@@ -0,0 +1,18 @@
+User.create(
+   nickname: "58b",
+   family_name: "真子".
+   family_name_kana: "真子",
+   first_name: "なり"
+   first_name_kana: "なり"
+   email: "aaa@aaa"
+   encrypted_password: "techexpe"
+   phone_number: 09001190110
+   profile: "サンプルです"
+   reset_password_token: ""
+   reset_password_sent_at: ""
+   remember_created_at: ""
+   created_at: "2020-02-13"
+   updated_att: "2020-02-13"
+   birthdayt: "1988-09-10"
+)
+   
