b774930ddf99cdc9197ba407b9bceb20bcd15e2e
takeda-shin <kgcfy958@yahoo.co.jp>
2020-02-29 16:02:29 +0900

    edit routes of proiduct_detail_page
    ・出品者が商品詳細ページを開く
    →編集・削除ボタンが出る
    ・他の人が開く
    →購入ボタンとコメント欄が出る
    
    この実装に

--- config/routes.rb
+++ config/routes.rb
@@ -29,12 +29,9 @@ Rails.application.routes.draw do
 
   resources :products do
     member do
-      get 'details'
       post 'products/new' => 'products#new'
     end
   end
-
-  get 'products/:id' => 'products#show'
 
   resources :users, only: [:index, :new, :show, :edit] do
     collection do
